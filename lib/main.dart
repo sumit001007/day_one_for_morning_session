3ff7d772dd302bf41c885affa64a7a94a90aa803
sumit001007 <sumitapp22@gmail.com>
2021-09-15 18:28:11 +0530

    after some practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +11,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'container',
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: const Text('Container Widget'),
+          backgroundColor: Colors.black,
+          leading: Icon(Icons.home),
+        ),
+        body: Container(
+          height: 100,
+          width: 100,
+          margin: EdgeInsets.all(20),
+          alignment: Alignment.center,
+          child: Text('hello world!!!'),
+          decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              border: Border.all(width: 4, color: Colors.black),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(color: Colors.orangeAccent, offset: Offset(6.0, 6.0))
+              ]),
+          transform: Matrix4.rotationZ(0.1),
+        ),
+      ),
     );
   }
 }
